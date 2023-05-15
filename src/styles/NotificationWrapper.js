0750158b5e92990242fb3e747c65373ab5c810b9
Angelique15 <113044854+Angelique15@users.noreply.github.com>
2023-05-15 23:43:45 +0000

    Deploying to gh-pages from @ Angelique15/goit-react-hw-04-feedback@1fa0b708ff92c431aa3f5fce99bcb0ff655dfad1 🚀

--- src/styles/NotificationWrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const NotificationWrapper = styled.p`
-  color: #4291F0;
-  font-size: 20px;
-  font-weight: bold;
-`;
-export default NotificationWrapper;
