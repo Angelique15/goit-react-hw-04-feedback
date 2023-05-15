2a4c69e78757763b76cee2ac68095f99776f0186
Angelique15 <mar.angel.go15@gmail.com>
2023-05-15 18:41:41 -0500

    initial commit

---
+++ src/components/Notification.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NotificationWrapper } from '../styles/NotificationWrapper';
+
+const Notification = ({ message }) => {
+  return <NotificationWrapper>{message}</NotificationWrapper>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default Notification;
+
+
+
